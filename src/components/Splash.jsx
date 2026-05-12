5083de954a775a720b5f564d2015fa0cfd9107c5
davidy <david.yang026@gmail.com>
2026-05-12 11:05:24 -0700

    updated wording

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -10,9 +10,9 @@ const navigate = useNavigate()
     <div style={styles.container} className="flex items-center justify-center">
       <div className="w-full max-w-lg rounded-2xl bg-white/80 p-8 shadow-xl backdrop-blur">
         <div className="text-4xl font-bold text-gray-900">Healthier Choice</div>
-        <p className="mt-4 text-sm text-gray-700" style={{ textAlign: "justify" }}>
-          Simply take a picture of the ingredients and the AI will help you identify any harmful ingredients. <br/>
-          <span className="text-xs">* For educational purposes only. </span>
+        <p className="mt-4 text-md text-gray-700" style={{ textAlign: "justify" }}>
+          Simply take a picture of the ingredients and the AI model will help you identify any harmful ingredients. <br/>
+          <span className="text-xs">* For educational purposes only. AI model will try to identify ingredients that are not in compliance to the EU Commision's Food Saftey Policy</span>
         </p>
 
         {/* navigate to camera page on click */}
